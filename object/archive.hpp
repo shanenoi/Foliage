a389b335016e3411d071905b72c1ebc7d76a2a16
shanenoi <shanenoi.org@gmail.com>
2020-09-26 01:46:10 +0700

    add file type detecter

--- object/archive.hpp
+++ object/archive.hpp
@@ -1,89 +1,12 @@
 #ifndef __ARCHIVE__
 #define __ARCHIVE__
 
-#include <cstdlib>
-#include <stdio.h>
-#include "file.hpp"
+#include "archive_app.hpp"
 
-class archive_format {
-
-    public:
-        const char* app_name;
-        const char* options;
-        const char* input_files;
-        const char* output_file;
-        char* temporary_var;
-
-        ~archive_format() {free(this->temporary_var);}
-
-        int check_available() {return system(this->app_name);}
-
-        int compress();
-
-        int extract();
-};
-
-
-class zip: public archive_format {
-
-    public:
-
-        int compress() {
-            this->app_name = "zip";
-            this->options = "-r";
-
-            this->temporary_var = (char*)malloc( strlen(this->app_name) + strlen(this->options) +
-                                                 strlen(this->output_file) + strlen(this->input_files));
-            sprintf(this->temporary_var, "%s %s %s %s", this->app_name, this->options,
-                                                        this->output_file, this->input_files);
-
-            return system(this->temporary_var);
-        }
-
-        int extract(const char* input_file) {
-            this->options = "-o";
-            this->temporary_var = this->temporary_var = (char*)malloc(strlen("unzip") + 
-                                                                      strlen(input_file) + 
-                                                                      strlen(this->options) + 2);
-            sprintf(this->temporary_var,
-                     "%s %s %s",
-                     "unzip", this->options, input_file);
-
-            return system(this->temporary_var);
-        }
-
-};
-
-
-class seven_zip: public archive_format {
-    public:
-
-        int compress() {
-            this->app_name = strdup("7z");
-            this->options = strdup("a");
-
-            this->temporary_var = (char*)malloc(sizeof(this->app_name)*4);
-            sprintf(this->temporary_var, "%s %s %s %s", this->app_name, this->options,
-                                                        this->output_file, this->input_files);
-            return system(this->temporary_var);
-        }
-
-        int extract(const char* input_file) {
-            this->options = "x -aoa";
-            this->temporary_var = (char*)malloc(strlen(this->app_name) + 2 +
-                                                strlen(this->options) +
-                                                strlen(input_file));
-            sprintf(this->temporary_var, "%s %s %s", this->app_name,
-                                                     this->options,
-                                                     input_file);
-            return system(this->temporary_var);
-        }
-};
-
-void* archive_open(char* path, char** start, char** end) {
+void* open_archive(char* path, char** start, char** end) {
     // this function will interac with file
-    printf("open %s\n", path);
-    return NULL;
+	printf("open %s\n", path);
+	return NULL;
 }
 
-#endif __ARCHIVE__
+#endif /*__ARCHIVE__*/
--- object/file.hpp
+++ object/file.hpp
@@ -2,17 +2,17 @@
 #define __INCLD_ININT__
 
 #include "object.hpp"
-#include "archive_app.hpp"
+#include "archive.hpp"
 #include "audio.hpp"
 #include "code.hpp"
 #include "image.hpp"
 #include "video.hpp"
-#include "web.hpp"
 
 #endif /*__INCLD_ININT__*/
 
 #ifndef __META_PATH__
 #define __META_PATH__
+
 
 typedef struct {
 
@@ -21,4 +21,84 @@ typedef struct {
 
 } meta_path;
 
+
 #endif /*__META_PATH__*/
+
+#ifndef __DETECT_TYPE__
+#define __DETECT_TYPE__
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h> 
+#include "types.hpp"
+
+class detecter {
+
+    private:
+    
+        int max_size = 13;
+        int *stor = (int*)malloc(sizeof(int)*this->max_size);
+        FILE *fp;
+
+
+    public:
+        const char *file;
+        char *extension = (char*)malloc(sizeof(char)*9);
+
+        detecter() {init_type();}
+        ~detecter() {free(this->stor);
+                     free_type();}
+        char *get_extension();
+        int compare(int*, int, int*);
+
+
+    private:
+    
+        void read() {
+            this->fp = fopen(this->file, "rb");
+            int single_char;
+            int index = 0;
+            while ((single_char=getc(fp))!= EOF && index!=this->max_size) {
+                this->stor[index++] = single_char;
+            }
+            fclose(this->fp);
+        }
+
+
+    public:
+
+        enum TYPES astype() {
+            this->read();
+            this->get_extension();
+            for (int i=0; i<DEFAULT_LEN; i++) {
+                if (this->compare(DEFAULT[i].header, DEFAULT[i].len, this->stor)) {
+                    if (DEFAULT[i].name==ZIP &&
+                        strcmp(this->extension, "docx")==0) {return DOCX;}
+                    return DEFAULT[i].name;
+                }
+            }
+            return TXT;
+        }
+
+};
+
+char *detecter::get_extension() {
+    detecter::extension = strdup("");
+    char buff[2] = {' ', '\0'};
+    int len_file_name = strlen(detecter::file);
+    for (int i=len_file_name-1; i!=0; i--) {
+        if (detecter::file[i]=='.' or detecter::file[i]=='/') break;
+        buff[0] = detecter::file[i];
+        detecter::extension = strcat(strdup(buff), detecter::extension);
+    }   
+    return detecter::extension;
+}
+
+int detecter::compare(int *header, int len_header, int *str2) {
+    for(int i=0; i<len_header; i++) {
+        if (header[i] != str2[i]) {return 0;}
+    }
+    return 1;
+}
+
+#endif /*__DETECT_TYPE__*/
